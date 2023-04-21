07caa7f3322b0cbb855018fd8055ad6bcaee2f03
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-04-20 23:53:05 -0300

    Feat: image test/models

--- app/core/models.py
+++ app/core/models.py
@@ -1,10 +1,21 @@
 """
 Database models.
 """
+import os
+import uuid
+
 from django.conf import settings
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
                                         PermissionsMixin)
 from django.db import models
+
+
+def recipe_image_file_path(instance, filename):
+    """Generete file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('updloads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -55,6 +66,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self) -> str:
         return self.title
